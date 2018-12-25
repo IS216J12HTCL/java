98724a168255aa838ca5c38d046bb98b9d426262
avatar123pro016 <46129302+avatar123pro016@users.noreply.github.com>
2018-12-25 18:31:26 +0700

    Duy Linh

--- StudentSystem/StudentSystem/src/main/java/vn/edu/uit/dashboard/controller/StudentController.java
+++ StudentSystem/StudentSystem/src/main/java/vn/edu/uit/dashboard/controller/StudentController.java
@@ -3,9 +3,18 @@
  */
 package vn.edu.uit.dashboard.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import vn.edu.uit.dashboard.dao.StudentDao;
+import vn.edu.uit.dashboard.model.Student;
 
 /**
  * @author Duy Linh
@@ -15,4 +24,14 @@ public class StudentController {
 	
 	@Autowired
 	private StudentDao studentDao;
+	
+	@RequestMapping(value = { "" }, method = RequestMethod.GET)
+	public String getCustomer(ModelMap model, HttpServletRequest request) {
+		
+	
+		List<Student> students = studentDao.findAll();
+		model.addAttribute("customers", students);
+		return "customers";
+	}
+
 }
